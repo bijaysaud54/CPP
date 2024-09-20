ea81266f0f7465bc1fb6d79dc10f0b35419eaf27
Bijay Saud <bijaysinghsaud@gmail.com>
2024-09-20 21:41:53 +0545

    OBJECT AND CLASS

---
+++ usingconstructors.cpp
@@ -0,0 +1,26 @@
+ #include<iostream>
+ using namespace std;
+ class point
+ {
+    int x,y;
+    public:
+    point(int x1,int y1)
+    {
+        x=x1;
+        y=y1;
+    }
+    int getx()
+    {
+        return x;
+    }
+    int gety()
+    {
+        return y;
+    }
+ };
+ int main()
+ {
+ point p1(20,15);
+ cout<<"p1.x="<<p1.getx()<<",p1.y="<<p1.gety();
+ return 0;
+ }
