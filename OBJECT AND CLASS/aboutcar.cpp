98c3555663701d4e0a9d2377b7a82f47db8fdee9
Bijay Saud <bijaysinghsaud@gmail.com>
2024-09-28 21:58:12 +0545

    AboutCar

--- OBJECT AND CLASS/aboutcar.cpp
+++ OBJECT AND CLASS/aboutcar.cpp
@@ -16,13 +16,13 @@ int main()
     car c1;
     c1.brand = "BMW";
     c1.model = "X6";
-    c1.year = "1996";
+    c1.year = "1999";
 
 
     car c2;
     c2.brand = "LAMBO";
     c2.model = "X9";
-    c2.year = "1920";
+    c2.year = "1969";
 
 
     cout<<c1.brand<<"Name "<<c1.model<<"model "<<c1.year<<"year "<<endl;
